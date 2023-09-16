36cf806cdeb4b18aa5c968d9c82e97bb66cbc340
karunakar Neravetla <kneravetla@users.noreply.github.com>
2023-09-16 15:09:21 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner');
 
-print ('Breakfast Menu');
+streamlit.header ('Breakfast Menu');
 
-print( "Omega 3 & BlueBerry OatMeal" );
+streamlit.text ( "Omega 3 & BlueBerry OatMeal" );
+streamlit.text ( "Kale, Spinach & Rocket Smoothie" );
+streamlit.text ( "Hard Boiled Free-Range Egg" );
