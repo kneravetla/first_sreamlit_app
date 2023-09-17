43e2a0479ce8140d962fdc3d1dd0d41210868821
karunakar Neravetla <kneravetla@users.noreply.github.com>
2023-09-17 19:25:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,13 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #display the table on page
 streamlit.dataframe(fruits_to_show);
 
+# New Section to display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice!');
+fruit_choice = streamlit.text_input('What Fruit would you like information about?', 'Kiwi');
+streamlit.write('The user entered',  fruit_choice )
+
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi");
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice);
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json());
 streamlit.dataframe(fruityvice_normalized);
